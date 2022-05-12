09684453e615a003a84baf41fd8421f35eaad57a
SorkerLimon <sorkerlimon18@gmail.com>
2022-05-12 18:36:24 +0600

    pyqt6

---
+++ pyqt6.py
@@ -0,0 +1,79 @@
+# from PyQt6.QtWidgets import QApplication,QWidget
+# import sys
+#
+# app = QApplication(sys.argv)
+#
+# window = QWidget()
+# window.setWindowTitle("Sorker Limon")
+# window.show()
+#
+# app.exec()
+
+
+##### Button created
+
+
+# import sys
+# from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+#
+#
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.setWindowTitle("My App")
+#
+#         button = QPushButton("Press Me!")
+#         button.setCheckable(True)
+#         button.clicked.connect(self.the_button_was_clicked)
+#         button.clicked.connect(self.the_button_was_toggled)
+#
+#         self.setCentralWidget(button)
+#
+#     def the_button_was_clicked(self):
+#         print("Clicked!")
+#
+#     def the_button_was_toggled(self, checked):
+#         print(checked)
+#
+#
+# app = QApplication(sys.argv)
+#
+# window = MainWindow()
+# window.show()
+#
+# app.exec()
+
+
+
+
+######## disable the button
+
+#
+# import sys
+# from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+#
+#
+# class MainWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.setWindowTitle("My App")
+#
+#         self.button = QPushButton("Press Me!")
+#         self.button.clicked.connect(self.the_button_was_clicked)
+#
+#         self.setCentralWidget(self.button)
+#
+#     def the_button_was_clicked(self):
+#         self.button.setText("You already clicked me.")
+#         self.button.setEnabled(False)
+#         self.setWindowTitle("My Oneshot App")
+#
+#
+# app = QApplication(sys.argv)
+#
+# window = MainWindow()
+# window.show()
+#
+# app.exec()
