030b5debd2ad06b111d9d1933e3e75ac5dbc1f37
SorkerLimon <sorkerlimon18@gmail.com>
2022-05-18 15:03:17 +0600

    git button add

--- pyqt6.py
+++ pyqt6.py
@@ -78,118 +78,163 @@
 #
 # app.exec()
 
+######################## Lima Project
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import json
+# import pandas as pd
+# from pandas import json_normalize
+# import os
+#
+# from PyQt6.QtWidgets import (QWidget, QPushButton, QLineEdit,
+#         QInputDialog, QApplication)
+# import sys
+#
+#
+# class Example(QWidget):
+#
+#     def __init__(self):
+#         super().__init__()
+#
+#         self.initUI()
+#
+#
+#     def initUI(self):
+#
+#         self.btn = QPushButton('Ok', self)
+#         self.btn.move(180, 220)
+#         self.btn.clicked.connect(self.showDialog)
+#
+#         self.le = QLineEdit(self)
+#         self.le.move(150, 150)
+#
+#
+#
+#         self.setGeometry(500, 500, 500, 500)
+#         self.setWindowTitle('Log Analysis System Path')
+#         self.show()
+#
+#
+#     def showDialog(self):
+#
+#         text, ok = QInputDialog.getText(self, 'Path Input',
+#                                         'Enter your Data Fle Path:')
+#
+#         if ok:
+#             self.le.setText(str(text))
+#
+#         if text:
+#
+#             path = open(os.path.expanduser(text))
+#
+#             # data = pd.read_csv("C:/Users/21100002/Desktop/sammiha.csv")
+#             # data = pd.read_csv(text)
+#             data = pd.read_csv(path)
+#             # print(data)
+#
+#             column = data.columns
+#             # print(column)
+#
+#             vmcapacity = data["Latency/vmcapacity"].sum()
+#             print("Totall vmcapacity : ", vmcapacity)
+#
+#             processedrequests = data["Latency/processedrequests"].sum()
+#             print("Totall processedrequests : ", processedrequests)
+#
+#             totalrentalcost = data["Latency/totalrentalcost"].sum()
+#             print("Totall totalrentalcost : ", totalrentalcost)
+#
+#             arrivalnewconnections = data["Latency/arrivalnewconnections"].sum()
+#             print("Totall arrivalnewconnections : ", arrivalnewconnections)
+#
+#             fig, axs = plt.subplots(2, 2)
+#             fig.suptitle('Log Analysis ')
+#
+#             axs[0][0].plot(data["Latency/vmcapacity"])
+#             axs[0, 0].set_title('Vm Capacity')
+#
+#             axs[0][1].plot(data["Latency/processedrequests"])
+#             axs[0, 1].plot(data["Latency/processedrequests"], 'tab:orange')
+#             axs[0, 1].set_title('Processed Requests')
+#
+#             axs[1][0].plot(data["Latency/totalrentalcost"])
+#             axs[1, 0].plot(data["Latency/totalrentalcost"], 'tab:green')
+#             axs[1, 0].set_title('Totalrental Cost')
+#
+#             axs[1][1].plot(data["Latency/arrivalnewconnections"])
+#             axs[1, 1].plot(data["Latency/arrivalnewconnections"], 'tab:red')
+#             axs[1, 1].set_title('Arrival New Connections')
+#
+#             plt.show()
+#
+#             print(path)
+#
+#
+# def main():
+#
+#     app = QApplication(sys.argv)
+#     ex = Example()
+#     sys.exit(app.exec())
+#
+#     obj = Example()
+#     filepath = obj.showDialog()
+#     print(filepath)
+#
+#
+#
+# if __name__ == '__main__':
+#     main()
+#
 
-import numpy as np
-import matplotlib.pyplot as plt
-import json
-import pandas as pd
-from pandas import json_normalize
-import os
 
-from PyQt6.QtWidgets import (QWidget, QPushButton, QLineEdit,
-        QInputDialog, QApplication)
+
+
+from PyQt6.QtWidgets import QApplication,QMainWindow,QPushButton
 import sys
+from random import choice
 
+window_titles = [
+    'My App',
+    'My software',
+    'My abrar',
+    'My tasnim',
+    'My nawshin',
+    'My rafi',
+    'My sorker',
+    'My sadia',
+    'My limon',
+    'Something went wrong',
+]
 
-class Example(QWidget):
-
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.n_times_clicked = 0
+        self.setWindowTitle("My Application")
+        self.button = QPushButton("Press Me!")
+        self.button.clicked.connect(self.the_button_was_clicked)
+        self.windowTitleChanged.connect(self.the_window_tittle_change)
+        self.setCentralWidget(self.button)
 
-        self.initUI()
-
-
-    def initUI(self):
-
-        self.btn = QPushButton('Ok', self)
-        self.btn.move(180, 220)
-        self.btn.clicked.connect(self.showDialog)
-
-        self.le = QLineEdit(self)
-        self.le.move(150, 150)
-
+    def the_button_was_clicked(self):
+        print("Clicked")
+        new_window_tittle = choice(window_titles)
+        self.setWindowTitle(new_window_tittle)
 
 
-        self.setGeometry(500, 500, 500, 500)
-        self.setWindowTitle('Log Analysis System Path')
-        self.show()
+    def the_window_tittle_change(self,window_tittle):
+        print("Window tittlechanged: %s "% window_tittle)
+
+        if window_tittle == "Something went wrong":
+            self.button.setDisabled(True)
 
 
-    def showDialog(self):
+app = QApplication(sys.argv)
 
-        text, ok = QInputDialog.getText(self, 'Path Input',
-                                        'Enter your Data Fle Path:')
+window = MainWindow()
+window.show()
 
-        if ok:
-            self.le.setText(str(text))
-
-        if text:
-
-            path = open(os.path.expanduser(text))
-
-            # data = pd.read_csv("C:/Users/21100002/Desktop/sammiha.csv")
-            # data = pd.read_csv(text)
-            data = pd.read_csv(path)
-            # print(data)
-
-            column = data.columns
-            # print(column)
-
-            vmcapacity = data["Latency/vmcapacity"].sum()
-            print("Totall vmcapacity : ", vmcapacity)
-
-            processedrequests = data["Latency/processedrequests"].sum()
-            print("Totall processedrequests : ", processedrequests)
-
-            totalrentalcost = data["Latency/totalrentalcost"].sum()
-            print("Totall totalrentalcost : ", totalrentalcost)
-
-            arrivalnewconnections = data["Latency/arrivalnewconnections"].sum()
-            print("Totall arrivalnewconnections : ", arrivalnewconnections)
-
-            fig, axs = plt.subplots(2, 2)
-            fig.suptitle('Log Analysis ')
-
-            axs[0][0].plot(data["Latency/vmcapacity"])
-            axs[0, 0].set_title('Vm Capacity')
-
-            axs[0][1].plot(data["Latency/processedrequests"])
-            axs[0, 1].plot(data["Latency/processedrequests"], 'tab:orange')
-            axs[0, 1].set_title('Processed Requests')
-
-            axs[1][0].plot(data["Latency/totalrentalcost"])
-            axs[1, 0].plot(data["Latency/totalrentalcost"], 'tab:green')
-            axs[1, 0].set_title('Totalrental Cost')
-
-            axs[1][1].plot(data["Latency/arrivalnewconnections"])
-            axs[1, 1].plot(data["Latency/arrivalnewconnections"], 'tab:red')
-            axs[1, 1].set_title('Arrival New Connections')
-
-            plt.show()
-
-            print(path)
-
-
-def main():
-
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec())
-
-    obj = Example()
-    filepath = obj.showDialog()
-    print(filepath)
-
-
-
-if __name__ == '__main__':
-    main()
-
-
-
-
-
-
+app.exec()
 
 
 
